fb6711820b12ac8eb7e12eef356bb57f04265934
RezaR2P <kecilreza575@gmail.com>
2022-10-19 17:08:29 +0700

    Pertama

---
+++ application/controllers/Auth.php
@@ -0,0 +1,62 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Auth extends CI_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('form_validation');
+    }
+    public function index()
+    {
+        $this->form_validation->set_rules('email', 'Email', 'trim|required');
+        $this->form_validation->set_rules('password', 'Password', 'trim|required');
+        if ($this->form_validation->run() == false) {
+            $data['title'] = 'Login page';
+            $this->load->view('templates/auth_header', $data);
+            $this->load->view('auth/index');
+            $this->load->view('templates/auth_footer');
+        } else {
+            $this->_login();
+        }
+    }
+    private function _login()
+    {
+        $email = $this->input->post('email');
+        $password = $this->input->post('password');
+
+        $user = $this->db->get_where('user', ['email' => $email])->row_array();
+
+        if ($user) {
+            if (password_verify($password, $user['password'])) {
+                $data = [
+                    'email' => $user['email'],
+                    'role_id' => $user['role_id'],
+                ];
+                $this->session->set_userdata($data);
+                if ($user['role_id'] == 1) {
+                    redirect('admin');
+                } else {
+                    redirect('manajemen');
+                }
+            } else {
+                $this->session->set_flashdata('msg_error', 'Password Salah');
+                redirect('auth');
+            }
+        } else {
+            $this->session->set_flashdata('msg_error', 'email tidak terdaftar');
+            redirect('auth');
+        }
+    }
+
+
+    public function logout()
+    {
+        $this->session->unset_userdata('email');
+        $this->session->unset_userdata('role_id');
+        $this->session->set_flashdata('msg_success', 'You have been logged out!');
+        redirect('auth');
+    }
+}
